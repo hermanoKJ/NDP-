962b7d82c8b365b38030dbb284f2076ffb273b0b
Seshie Herbert Kojo <herbertkojo11@gmail.com>
2026-05-29 20:02:19 +0100

    Variables have been included incase you want to make some changes
    line 17, send has been changed to sendall

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 import socket
 import os
 
+# Configuration variables make it easier to update later
+HOST = 'localhost'
+PORT = 5001
+FILE_TO_SEND = "school pic.jpeg"
+BUFFER_SIZE = 4096
+
+#1. Initialize and connect
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 5001))
 
 file_to_send = "school pic.jpeg" 
 
-client_socket.send(f"{file_to_send}<NAME_END>".encode())
+#2. Send filename with delimiter (using sendall)
+client_socket.sendall(f"{file_to_send}<NAME_END>".encode('utf-8'))
 
+#
 with open(file_to_send, 'rb') as file:
     print(f"Sending {file_to_send}...")
     data = file.read(1024)
